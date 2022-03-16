51045eaaa184babeedff3df0422e81f1cf6e9f6b
Carlos Loaiza <carloaiza@umanizales.edu.co>
2022-03-15 21:47:05 -0500

    Crud completo

--- service/user_service.py
+++ service/user_service.py
@@ -1,9 +1,27 @@
 from config.mongo_db import MongoApi
 
 class UserService:
+    data = {"database":"arepitas_ricas",
+                         "collection":"users"}
+
     def get_all_users(self):
-        connection = MongoApi({"database":"arepitas_ricas",
-                         "collection":"users"})
+        connection = MongoApi(self.data)
         response = connection.read()
         return response
 
+    def save_user(self,data):
+        connection = MongoApi(self.data)
+        response = connection.write(data)
+        return response
+
+    def update_user(self, data):
+        self.data['Filter']=data['Filter']
+        self.data['DataToBeUpdated'] = data['DataToBeUpdated']
+        connection = MongoApi(self.data)
+        response = connection.update()
+        return response
+
+    def delete_user(self, data):
+        connection = MongoApi(self.data)
+        response = connection.delete(data)
+        return response
